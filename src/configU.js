7acadb5ea09e511f921242c1e9629357a1260b95
Rahul Raman <rahul@bewgle.com>
2020-12-21 21:01:37 +0530

    restart service if there is a change in version/list of files

--- src/configU.js
+++ src/configU.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const sh = require('shelljs');
 
+const configCacheRoot = `${process.env.HOME}/.cache/configU`;
+
 //============= File related methods =================
 exports.saveFileContent = (filepath, content) => {
     try {
@@ -46,13 +48,96 @@ exports.writeFileOwner = (filepath, uid, gid) => {
 
 
 // =========  PACKAGE related methods =================
-exports.installPackage = function install (pkg) {
+exports.installPackage = (pkg) => {
     console.log(`Installing ${pkg}`);
     sh.exec(`sudo apt-get -y install ${pkg}`);
 };
   
-exports.removePackage = function remove (pkg) {
+exports.removePackage = (pkg) => {
     console.log(`Removing ${pkg}`);
     sh.exec(`sudo apt-get -y remove ${pkg}`);
 };
+
+
+exports.restartService = (service, files = []) => {
+     /**
+      * Restart a serice when relavant files change.
+      * Compare previously cached version of package or file metadata
+      *  to check if there's been a change
+      */
+  
+    // If any file in the array `files` returns true, restart the service.
+    let fileChanged;
+    files.forEach(function (file) {
+      if (hasFileChanged(file)) {
+        fileChanged = true;
+      }
+    });
+  
+    var cache = `${configCacheRoot}/${service}.cache`;
+    var shouldRestart = false;
+  
+    if (fileChanged) {
+      shouldRestart = true;
+    }
+    if (!fs.existsSync(cache)) {
+      console.log(`Cache dir not found for ${service}.`);
+      console.log('Creating config cache directory...');
+  
+      mkdirp.sync(configCacheRoot);
+      cacheServiceVersion(service);
+      shouldRestart = true;
+    } else {
+      var cacheData = fs.readFileSync(cache, 'utf8');
+      var checkPackageData = sh.exec(`apt list ${service}`);
+        
+
+      if (cacheData !== checkPackageData.toString()) {
+        console.log(`Package version for ${service} changed.`);
+        cacheServiceVersion(service);
+        shouldRestart = true;
+      }
+    }
+    if (shouldRestart) {
+      console.log(`Restarting ${service}.`);
+      sh.exec(`sudo service ${service} restart`);
+    }
+  };
+  
+function hasFileChanged (file) {
+    if (fs.existsSync(file)) {
+        var fileStats = fs.statSync(file);
+        var cacheTime = readCacheModifyTime(file);
+
+        if (fileStats.mtime.toString() !== cacheTime) {
+            cacheFileModifyTime(file);
+            return true;
+        }
+    }
+    return false;
+}
+  
+  function cacheFileModifyTime (filepath) {
+    var modStats = fs.statSync(filepath);
+    var cacheName = filepath.replace(/\//g, '-');
+  
+    mkdirp.sync(configCacheRoot);
+    fs.writeFileSync(`${configCacheRoot}/${cacheName}.mtime`, modStats.mtime);
+  }
+  
+  function readCacheModifyTime (filepath) {
+    var cacheName = filepath.replace(/\//g, '-');
+    var cacheFilePath = `${configCacheRoot}/${cacheName}.mtime`;
+  
+    if (fs.existsSync(cacheFilePath)) {
+      var cacheContents = fs.readFileSync(cacheFilePath, 'utf8');
+      return cacheContents;
+    }
+  }
+  
+  // get service's package version and store it in cache
+  function cacheServiceVersion (service) {
+    let serviceCache = sh.exec(`apt list ${service}`);
+    fs.writeFileSync(`${configCacheRoot}/${service}.cache`, serviceCache);
+  }
 
